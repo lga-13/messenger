1c16da8f8d08008eaa9526eb3261c47cb0464312
glazarev <gleb.lazarev20@yandex.ru>
2024-04-04 23:16:47 +0300

    Работает login-form

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,15 @@ import SettingsPage from "./pages/settings-page/settings-page.ts";
 import Error404Page from "./pages/error404/error404.ts";
 import Error500Page from "./pages/error500/error500.ts";
 import RegistrationForm from "./pages/registration-form/registration-form.ts";
+import LoginForm from "./pages/login-form/login-form.ts";
 
 const router = new Router('#app')
     .use("/404", Error404Page, {})
     .use("/505", Error500Page, {})
     .use("/settings", SettingsPage, {})
-    .use("/", ChatPage, {})
+    .use("/messenger", ChatPage, {})
     .use("/sign-up", RegistrationForm, {})
+    .use("/", LoginForm, {})
 
 router.start()
+
