b05d8e446b508fb44948600026eb73ab5e56a0bd
glazarev <gleb.lazarev20@yandex.ru>
2024-04-22 17:05:59 +0300

    работает доабвление пользака в чат

--- src/api/chats-api.ts
+++ src/api/chats-api.ts
@@ -2,26 +2,16 @@ import HTTP from "../modules/http/http.ts";
 import {BaseAPI} from "./base-api.ts";
 import {API_DOMAIN, BASE_PATH} from "../../settings.ts";
 import {
-    ArchiveChatDataType,
-    ArchiveChatsGetDataType, AvatarChatDataType,
     ChatCreateDataType,
-    ChatsGetDataType, ChatUsersGetDataType,
-    DeleteChatDataType, UnarchiveChatDataType
+    ChatsGetDataType,
+    ChatUsersGetDataType,
+    DeleteChatDataType,
 } from "./api-types.ts";
-import {data} from "autoprefixer";
-import {c} from "vite/dist/node/types.d-aGj9QkWt";
 
 
 const chatsAPIPath = `${BASE_PATH}/chats`
-const chatsArchiveAPIPath = `${chatsAPIPath}/archive`
-const chatsUnarchiveAPIPath = `${chatsAPIPath}/unarchive`
-const chatsAvatarAPIPath = `${chatsAPIPath}/avatar`
-
 
 const chatAPIInstance = new HTTP(HTTP.prefixes.HTTPS, API_DOMAIN, 80, chatsAPIPath);
-const chatArchiveAPIInstance = new HTTP(HTTP.prefixes.HTTPS, API_DOMAIN, 80, chatsArchiveAPIPath);
-const chatUnarchiveAPIInstance = new HTTP(HTTP.prefixes.HTTPS, API_DOMAIN, 80, chatsUnarchiveAPIPath);
-const chatUserAPIInstance = new HTTP(HTTP.prefixes.HTTPS, API_DOMAIN, 80, chatsAPIPath);
 
 
 export class ChatsApi extends BaseAPI {
@@ -63,55 +53,9 @@ export class ChatsApi extends BaseAPI {
             {},
             {
                 "chatId": data.chatId,
-            }
-        )
-            .catch(error => {throw error});
-    }
-}
-
-export class ChatsArchiveApi extends BaseAPI {
-
-    create(data: ArchiveChatDataType) {
-        return chatArchiveAPIInstance.post(
-            "/",
-            {},
-            {},
-            {
-                "chatId": data.chatId,
-            }
-        )
-            .then(get_result => {return get_result})
-            .catch(error => {throw error});
-    }
-
-    request(data: ArchiveChatsGetDataType) {
-        return chatArchiveAPIInstance.get(
-            '/',
-            {
-                "offset": data.offset,
-                "limit": data.limit,
-                "title": data.title,
             },
-            {}
+            true
         )
-            .then(get_result => {return get_result})
-            .catch(error => {throw error});
-    }
-}
-
-
-export class ChatsUnarchiveApi extends BaseAPI {
-
-    create(data: UnarchiveChatDataType) {
-        return chatUnarchiveAPIInstance.post(
-            "/",
-            {},
-            {},
-            {
-                "chatId": data.chatId,
-            }
-        )
-            .then(get_result => {return get_result})
             .catch(error => {throw error});
     }
 }
@@ -120,7 +64,7 @@ export class ChatsUnarchiveApi extends BaseAPI {
 export class ChatUsersApi extends BaseAPI {
 
     request(data: ChatUsersGetDataType) {
-        return chatUserAPIInstance.get(
+        return chatAPIInstance.get(
             `${data.id}/users`,
             {
                 "offset": data.offset,
@@ -135,18 +79,33 @@ export class ChatUsersApi extends BaseAPI {
             .then(get_result => {return get_result})
             .catch(error => {throw error});
     }
+
+    replace() {
+        return chatAPIInstance.put(
+            "users",
+            {},
+            {},
+            {
+                "users": [
+                    290
+                ],
+                "chatId": 2968
+            },
+            true
+        )
+    }
+
 }
+
 
 const chatsApiInstance = new  ChatsApi();
 const chatUserApi = new ChatUsersApi()
-// console.log(await chatsApiInstance.create({
-//     title: "test"
-// }));
 console.log(await chatsApiInstance.request(
     {
         "limit": "10", "offset": "0", "title": ""
     }
 ))
+await chatUserApi.replace()
 console.log(await chatUserApi.request({
     id: 2968,
     limit: "10",
